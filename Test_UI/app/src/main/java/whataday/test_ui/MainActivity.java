70f7c2ddf1cc6516462382a5afbd7279556f2882
huni <masd6444@naver.com>
2016-02-21 23:36:50 +0900

    Bitmap 이용으로 변경 -> 문제 많음

--- Test_UI/app/src/main/java/whataday/test_ui/MainActivity.java
+++ Test_UI/app/src/main/java/whataday/test_ui/MainActivity.java
@@ -1,9 +1,14 @@
 package whataday.test_ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AbsListView;
+import android.widget.ImageView;
 
 import com.nirhart.parallaxscroll.views.ParallaxListView;
 
@@ -17,13 +22,24 @@ public class MainActivity extends AppCompatActivity {
     ListAdapter adapter;
     int focus;
 
+    ImageView image_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image_test = (ImageView)findViewById(R.id.image_test);
+
+        Bitmap bitmap_image = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
+        bitmap_image = BitmapUtil.cropCenterBitmap(bitmap_image, bitmap_image.getWidth(), 800);
+        Drawable b = new BitmapDrawable(getResources(), bitmap_image);
+
+        image_test.setBackground(b);
+
         startActivity(new Intent(getApplicationContext(), ScrollviewActivity.class));
 
+        /*
         arrdatas = new ArrayList<ListData>();
 
         for(int i = 1; i < 4; i++){
@@ -48,6 +64,6 @@ public class MainActivity extends AppCompatActivity {
         });
         parallaxListView.setAdapter(adapter);
 
-
+*/
     }
 }
--- Test_UI/app/src/main/java/whataday/test_ui/ScrollviewActivity.java
+++ Test_UI/app/src/main/java/whataday/test_ui/ScrollviewActivity.java
@@ -1,20 +1,20 @@
 package whataday.test_ui;
 
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ScrollviewActivity extends AppCompatActivity {
 
@@ -26,17 +26,17 @@ public class ScrollviewActivity extends AppCompatActivity {
 
     int scroll_max_y;
     int linear_max, content_max;
+    int focus_item;
 
-    RelativeLayout.LayoutParams text_param_country =
-            new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    Bitmap bitmap_image;
 
-    RelativeLayout.LayoutParams text_param_city =
-            new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
+    ImageView[] imageView_background = new ImageView[3];
     TextView[] textView_Country = new TextView[3];
     TextView[] textView_City = new TextView[3];
     RelativeLayout[] content_layout = new RelativeLayout[3];
     LinearLayout.LayoutParams[] content_layout_param = new LinearLayout.LayoutParams[3];
+    RelativeLayout.LayoutParams[] country_textview_param = new RelativeLayout.LayoutParams[3];
+    RelativeLayout.LayoutParams[] city_textview_param = new RelativeLayout.LayoutParams[3];
 
 
     @Override
@@ -56,33 +56,50 @@ public class ScrollviewActivity extends AppCompatActivity {
         content = (ScrollView)findViewById(R.id.scrollview);
         scroll_linear = (LinearLayout)findViewById(R.id.scroll_linear);
 
-        content_layout_param[0] = new LinearLayout.LayoutParams(view_width, content_height[0]);
-        content_layout_param[1] = new LinearLayout.LayoutParams(view_width, content_height[1]);
-        content_layout_param[2] = new LinearLayout.LayoutParams(view_width, content_height[2]);
-
+        focus_item = 0;
 
         for(int i=0; i<3; i++){
 
+            country_textview_param[i] =
+                    new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            city_textview_param[i] =
+                    new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            content_layout_param[i] = new LinearLayout.LayoutParams(view_width, content_height[i]);
+            //Vertical로 선언된 LinearLayout에 들어갈 각 content(RelativeLayout)의 초기 Param설정(크기).
+
             content_layout[i] = new RelativeLayout(this);
-            scroll_linear.addView(content_layout[i], content_layout_param[i]);
-            content_layout[i].setBackgroundResource(R.drawable.test2);
+//            content_layout[i].setBackgroundResource(R.drawable.test2);
+            //content(RelativeLayout)생성 후 백그라운드 이미지 설정.
+            imageView_background[i] = new ImageView(this);
+            imageView_background[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView_background[i].setBackgroundResource(R.drawable.test3);
+            imageView_background[i].setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            content_layout[i].addView(imageView_background[i]);
 
             textView_Country[i] = new TextView(this);
-            textView_Country[i].setText("Country");
+            textView_Country[i].setText("Country"+i);
             textView_Country[i].setTextSize(50);
-            textView_Country[i].setId(i);
-            text_param_country.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
+            textView_Country[i].setId(i+1);
+            country_textview_param[i].addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+            textView_Country[i].setLayoutParams(country_textview_param[i]);
+            //content의 중앙에 위치하게끔 Country Param을 설정.
+            //Country텍스트뷰에 설정한 Param을 설정.
             content_layout[i].addView(textView_Country[i]);
-            textView_Country[i].setLayoutParams(text_param_country);
+            //content에 country Add
 
             textView_City[i] = new TextView(this);
-            textView_City[i].setText("CITY");
+            textView_City[i].setText("CITY"+i);
             textView_City[i].setTextSize(30);
-            text_param_city.addRule(RelativeLayout.BELOW, i);
-            text_param_city.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+            city_textview_param[i].addRule(RelativeLayout.BELOW, i+1);
+            city_textview_param[i].addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+            textView_City[i].setLayoutParams(city_textview_param[i]);
+            //Country 텍스트뷰 아래쪽에 위치하게끔 City텍스트뷰 Param을 설정
             content_layout[i].addView(textView_City[i]);
-            textView_City[i].setLayoutParams(text_param_city);
+            //content에 city Add
+
+            scroll_linear.addView(content_layout[i], content_layout_param[i]);
+            //Linear에 content Add
 
         }
 
@@ -90,9 +107,11 @@ public class ScrollviewActivity extends AppCompatActivity {
         scroll_linear.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+                /*
                 Log.i("ssSIZE h:", String.valueOf(scroll_linear.getHeight()));
                 Log.i("ssSIZE w:", String.valueOf(scroll_linear.getWidth()));
                 Log.i("ssSIZE max:", String.valueOf(scroll_linear.getHeight() - view_height));
+                */
                 linear_max = scroll_linear.getHeight() - view_height;
             }
         });
@@ -100,23 +119,25 @@ public class ScrollviewActivity extends AppCompatActivity {
         content.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+                /*
                 Log.i("SIZE h:", String.valueOf(content.getHeight()));
                 Log.i("SIZE w:", String.valueOf(content.getWidth()));
                 Log.i("SIZE max:", String.valueOf(view_height - content.getHeight()));
+                */
                 content_max = view_height - content.getHeight();
-
                 scroll_max_y = linear_max + content_max;
-                Log.i("scroll_max_y :", String.valueOf(scroll_max_y));
 
             }
         });
 
+        bitmap_image = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
 
         content.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
 
                 int current_y = content.getScrollY();
+
                 if (current_y < scroll_max_y / 2) {
                     //첫번째_decrease / 두번째_increase / 세번째 고정
                     //첫번째 = width~width/2 : 0~scroll_max_y/2
@@ -125,43 +146,55 @@ public class ScrollviewActivity extends AppCompatActivity {
                     content_height[1] = convert_int_map(current_y, 0, scroll_max_y/2, view_width/2, view_width);
                     //세번째 = width/2
                     content_height[2] = view_width/2;
-                    Log.i("One :", String.valueOf(content_height[0])+"/"+String.valueOf(content_height[1])+"/"+String.valueOf(content_height[2]));
+
+                    if(content_height[0] < content_height[1]){
+                        focus_item = 1;
+                    }else{
+                        focus_item = 0;
+                    } //포커스 이동
+                    //Log.i("One :", String.valueOf(content_height[0])+"/"+String.valueOf(content_height[1])+"/"+String.valueOf(content_height[2]));
                 } else if (current_y > scroll_max_y / 2) {
                     //첫번째_고정 / 두번째_decrease / 세번째_increase
                     //첫번째 = width/2 : scroll_max_y/2~scroll_max_y
+                    if(current_y > scroll_max_y)
                     content_height[0] = view_width/2;
                     //두번째 = width~width/2 : scroll_max_y/2~scroll_max_y
                     content_height[1] = convert_int_map(current_y, scroll_max_y/2, scroll_max_y, view_width, view_width/2);
                     //세번째 = width/2~width : scroll_max_y/2~scroll_max_y
                     content_height[2] = convert_int_map(current_y, scroll_max_y/2, scroll_max_y, view_width/2, view_width);
-                    Log.i("Two :", String.valueOf(content_height[0])+"/"+String.valueOf(content_height[1])+"/"+String.valueOf(content_height[2]));
+
+                    if(content_height[1] < content_height[2]){
+                        focus_item = 2;
+                    }else{
+                        focus_item = 1;
+                    }
+                   // Log.i("Two :", String.valueOf(content_height[0])+"/"+String.valueOf(content_height[1])+"/"+String.valueOf(content_height[2]));
+
                 }
+
 
                 for(int i=0; i<3; i++){
 
                     content_layout_param[i].height = content_height[i];
                     content_layout[i].setLayoutParams(content_layout_param[i]);
-                    textView_City[i].setLayoutParams(text_param_city);
-                    textView_Country[i].setLayoutParams(text_param_country);
+
+                    textView_Country[i].setLayoutParams(country_textview_param[i]);
+                    textView_City[i].setLayoutParams(city_textview_param[i]);
+                    imageView_background[i].setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+                    int resize_height = convert_int_map(content_height[i], view_width / 2, view_width, bitmap_image.getHeight() / 2, bitmap_image.getHeight());
+
+                    Bitmap crop_bitmap = BitmapUtil.cropCenterBitmap(bitmap_image, bitmap_image.getWidth(), resize_height);
+                    Drawable b = new BitmapDrawable(getResources(), crop_bitmap);
+
+                    imageView_background[i].setBackground(b);
+
                 }
 
-                /*
-                content_layout_param[0].height = content_height[0];
-                content_layout_param[1].height = content_height[1];
-                content_layout_param[2].height = content_height[2];
-
-                content_layout[0].setLayoutParams(content_layout_param[0]);
-                content_layout[1].setLayoutParams(content_layout_param[1]);
-                content_layout[2].setLayoutParams(content_layout_param[2]);
-
-                textView_City[i].setLayoutParams(text_param_city);
-                textView_Country[i].setLayoutParams(text_param_country);
-*/
+                Log.i("FOCUS :", String.valueOf(focus_item));
 
             }
         });
-
-
     }
 
     int convert_int_map(int input, int input_min, int input_max, int convert_min, int convert_max){
